e870df3215d43fe1304c5629997787d26c91094f
agim <agimagba1990@gmail.com>
2024-08-27 12:21:48 +0100

    fixed when a negative quantity is placed and improved the alert message

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 
-quantity_ordered = document.getElementById('quantity');
-order_amount = document.getElementById('amount');
+const quantity_ordered = document.getElementById('quantity');
+const order_amount = document.getElementById('amount');
+let unit_price = 100;
 
 function place_order() {
-    alert('Your order has been received, and will be delivered to you shortly');
+    alert(`Your order for ${quantity_ordered.value} packs of peanuts, and will be delivered to you shortly`);
 
 }
+
+quantity_ordered.addEventListener('input', () => {
+    if (quantity_ordered.value < 0) {
+        unit_price = 0;
+    }
+    else {
+        unit_price = 100;
+    }
+    const calculated_price = quantity_ordered.value * unit_price;
+    order_amount.innerHTML = calculated_price
+});
