e37f912d60c6e8418c9a58133e9533d17eb188d9
agim <agimagba1990@gmail.com>
2024-08-27 11:39:35 +0100

    added some JS to cal the amount of orders made

---
+++ index.js
@@ -0,0 +1,8 @@
+
+quantity_ordered = document.getElementById('quantity');
+order_amount = document.getElementById('amount');
+
+function place_order() {
+    alert('Your order has been received, and will be delivered to you shortly');
+
+}
